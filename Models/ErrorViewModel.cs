68ba6d1e2c22807c2a27c86e00e8638f4841ed5c
daniquinto <dany.quintorios@gmail.com>
2026-05-21 20:32:59 -0500

    migrate: ASP.NET MVC to React + Vite

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace punto_fisico_app.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
